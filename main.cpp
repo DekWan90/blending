eff6cc98a39897410f91b122a5f098796fc28edf
Izwan Azmi <izwanazmi90@gmail.com>
2019-12-10 22:26:49 +0800

    Insert code

---
+++ main.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file main.cpp
+ * @brief Simple linear blender ( dst = alpha * src1 + beta * src2 )
+ * @author OpenCV team
+ */
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <iostream>
+
+// we're NOT "using namespace std;" here, to avoid collisions between the beta variable and std::beta in c++17
+using std::cin;
+using std::cout;
+using std::endl;
+
+/**
+ * @function main
+ * @brief Main function
+ */
+int main( void )
+{
+    double alpha = 0.5;
+    double beta;
+    double input;
+
+    cv::Mat src1, src2, dst;
+
+    /// Ask the user enter alpha
+    cout << " Simple Linear Blender " << endl;
+    cout << "-----------------------" << endl;
+    cout << "* Enter alpha [0.0-1.0]: ";
+    cin >> input;
+
+    // We use the alpha provided by the user if it is between 0 and 1
+    if( input >= 0 && input <= 1 )
+    {
+        alpha = input;
+    }
+
+    //![load]
+    /// Read images ( both have to be of the same size and type )
+    src1 = cv::imread( cv::samples::findFile( "LinuxLogo.jpg" ) );
+    src2 = cv::imread( cv::samples::findFile( "WindowsLogo.jpg" ) );
+    //![load]
+
+    if( src1.empty() )
+    {
+        cout << "Error loading src1" << endl;
+        return EXIT_FAILURE;
+    }
+    if( src2.empty() )
+    {
+        cout << "Error loading src2" << endl;
+        return EXIT_FAILURE;
+    }
+
+    //![blend_images]
+    beta = ( 1.0 - alpha );
+    cv::addWeighted( src1, alpha, src2, beta, 0.0, dst);
+    //![blend_images]
+
+    //![display]
+    cv::imshow( "Linear Blend", dst );
+    cv::waitKey(0);
+    //![display]
+
+    return 0;
+}
